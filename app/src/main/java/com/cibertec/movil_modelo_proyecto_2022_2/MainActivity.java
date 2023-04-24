3f674dbb13142e76cbce6199c932a6fec281fbbd
JeffJoelAmayaDominguez <jeffamayadominguez@hotmail.com>
2023-04-23 22:34:39 -0500

    PC1 Registrar Revista Jeff Amaya

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/MainActivity.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/MainActivity.java
@@ -2,7 +2,7 @@ package com.cibertec.movil_modelo_proyecto_2022_2;
 
 import android.os.Bundle;
 
-import com.cibertec.movil_modelo_proyecto_2022_2.util.NewAppCompatActivity;
+import com.cibertec.movil_modelo_proyecto_2022_2.entity.util.NewAppCompatActivity;
 
 
 public class MainActivity extends NewAppCompatActivity {
