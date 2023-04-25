4b6c9ab9ee1c05569119f9da70f73eb1e3a830e7
rrosasg <rrosasgranda@gmail.com>
2023-04-24 23:55:15 -0500

    Subir cambios de proveedor - registrar

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/MainActivity.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/MainActivity.java
@@ -1,8 +1,34 @@
 package com.cibertec.movil_modelo_proyecto_2022_2;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.cibertec.movil_modelo_proyecto_2022_2.entity.Pais;
+import com.cibertec.movil_modelo_proyecto_2022_2.entity.Proveedor;
+import com.cibertec.movil_modelo_proyecto_2022_2.entity.TipoProveedor;
+import com.cibertec.movil_modelo_proyecto_2022_2.service.ServicePais;
+import com.cibertec.movil_modelo_proyecto_2022_2.service.ServiceProveedor;
+import com.cibertec.movil_modelo_proyecto_2022_2.service.ServiceTipoProveedor;
+import com.cibertec.movil_modelo_proyecto_2022_2.util.ConnectionRest;
 import com.cibertec.movil_modelo_proyecto_2022_2.util.NewAppCompatActivity;
+import com.cibertec.movil_modelo_proyecto_2022_2.util.ValidacionUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class MainActivity extends NewAppCompatActivity {
@@ -11,7 +37,7 @@ public class MainActivity extends NewAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
-
-
 }
--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/service/ServiceProveedor.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/service/ServiceProveedor.java
@@ -1,4 +1,12 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.service;
 
+import com.cibertec.movil_modelo_proyecto_2022_2.entity.Proveedor;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface ServiceProveedor {
+    @POST("proveedor")
+    public abstract Call<Proveedor> insertaProveedor(@Body Proveedor objProveedor);
 }
