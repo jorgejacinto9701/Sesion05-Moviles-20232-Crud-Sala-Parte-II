b73d5781cd113fd700f8017e7a6d397a00446aec
AlanisGR <luzalanisGR@gmail.com>
2023-04-25 15:34:53 -0500

    Servicio libro

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/service/ServiceLibro.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/service/ServiceLibro.java
@@ -1,4 +1,8 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.service;
-
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
 public interface ServiceLibro {
+    @POST("libro")
+    public abstract Call<Libro> ingresarLibro(@Body Libro objLibro);
 }
