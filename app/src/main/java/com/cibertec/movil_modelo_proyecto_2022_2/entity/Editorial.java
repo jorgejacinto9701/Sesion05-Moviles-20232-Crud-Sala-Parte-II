0029ed823e001435e21269aa9e785ef1855fa803
CristianAquice <c23200112@gmail.com>
2023-04-27 03:51:12 -0500

    Cambios en Entidad Editorial

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/entity/Editorial.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/entity/Editorial.java
@@ -1,6 +1,76 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.entity;
 
 public class Editorial {
-//Probando
+    private int idEditorial;
+    private String razonSocial;
+    private String direccion;
+    private String ruc;
+    private String fechaCreacion;
+    private String fechaRegistro;
+    private int estado;
+    private Pais pais;
 
+    public int getIdEditorial() {
+        return idEditorial;
+    }
+
+    public void setIdEditorial(int idEditorial) {
+        this.idEditorial = idEditorial;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(String fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
 }
