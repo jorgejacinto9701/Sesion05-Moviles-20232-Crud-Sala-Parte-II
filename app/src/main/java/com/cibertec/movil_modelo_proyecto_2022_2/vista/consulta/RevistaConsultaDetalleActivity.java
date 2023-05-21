f1765677a9daf2a30d7a23d95c5bc312b329cfae
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-05-20 22:15:48 -0500

    Ajustes PC02

---
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/consulta/RevistaConsultaDetalleActivity.java
@@ -0,0 +1,16 @@
+package com.cibertec.movil_modelo_proyecto_2022_2.vista.consulta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.cibertec.movil_modelo_proyecto_2022_2.R;
+import com.cibertec.movil_modelo_proyecto_2022_2.util.NewAppCompatActivity;
+
+public class RevistaConsultaDetalleActivity extends NewAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_revista_consulta_detalle);
+    }
+}
---
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/consulta/UsuarioConsultaDetalleActivity.java
@@ -0,0 +1,14 @@
+package com.cibertec.movil_modelo_proyecto_2022_2.vista.consulta;
+
+import android.os.Bundle;
+import com.cibertec.movil_modelo_proyecto_2022_2.R;
+import com.cibertec.movil_modelo_proyecto_2022_2.util.NewAppCompatActivity;
+
+public class UsuarioConsultaDetalleActivity extends NewAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_consulta_detalle);
+    }
+}
