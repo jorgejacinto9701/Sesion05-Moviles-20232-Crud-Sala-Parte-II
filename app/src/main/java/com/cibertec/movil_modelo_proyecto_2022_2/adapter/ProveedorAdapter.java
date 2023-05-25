c9155e7d78f92ed272df44398d552514de6ce14f
rrosasg <rrosasgranda@gmail.com>
2023-05-25 02:54:26 -0500

    Listado Proveedores

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/adapter/ProveedorAdapter.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/adapter/ProveedorAdapter.java
@@ -1,10 +1,16 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.cibertec.movil_modelo_proyecto_2022_2.R;
 import com.cibertec.movil_modelo_proyecto_2022_2.entity.Proveedor;
 
 import java.util.List;
@@ -18,6 +24,29 @@ public class ProveedorAdapter extends ArrayAdapter<Proveedor>  {
         super(context, resource, lista);
         this.context = context;
         this.lista = lista;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.activity_proveedor_item, parent, false);
+
+        TextView txtIdProveedor = row.findViewById(R.id.txtIdProveedor);
+        TextView txtRazonSocial = row.findViewById(R.id.txtRazonSocial);
+        TextView txtRuc = row.findViewById(R.id.txtRuc);
+        TextView txtDireccion = row.findViewById(R.id.txtDireccion);
+        TextView txtFechaRegistro = row.findViewById(R.id.txtFechaRegistro);
+
+        Proveedor objProveedor = lista.get(position);
+        txtIdProveedor.setText(String.valueOf(objProveedor.getIdProveedor()));
+        txtRazonSocial.setText(objProveedor.getRazonsocial());
+        txtRuc.setText(objProveedor.getRuc());
+        txtDireccion.setText(objProveedor.getDireccion());
+        txtFechaRegistro.setText(objProveedor.getFechaRegistro());
+
+
+        return row;
     }
 
 }
--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/entity/TipoProveedor.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/entity/TipoProveedor.java
@@ -1,6 +1,8 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.entity;
 
-public class TipoProveedor {
+import java.io.Serializable;
+
+public class TipoProveedor implements Serializable {
 
     private int idTipoProveedor;
     private String  descripcion;
