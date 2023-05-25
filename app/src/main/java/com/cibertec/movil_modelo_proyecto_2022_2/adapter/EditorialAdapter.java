6ef4b8ad0ea43905f51d0d55a4eb4b2988a25d26
CristianAquice <c23200112@gmail.com>
2023-05-24 23:22:08 -0500

    PC 02 - Editorial Consultar

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/adapter/EditorialAdapter.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/adapter/EditorialAdapter.java
@@ -1,10 +1,16 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.cibertec.movil_modelo_proyecto_2022_2.R;
 import com.cibertec.movil_modelo_proyecto_2022_2.entity.Editorial;
 
 import java.util.List;
@@ -18,6 +24,35 @@ public class EditorialAdapter extends ArrayAdapter<Editorial>  {
         super(context, resource, lista);
         this.context = context;
         this.lista = lista;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.activity_editorial_item, parent, false);
+
+        Editorial objEditorial = lista.get(position);
+
+        TextView txtID = row.findViewById(R.id.DetalleIDEditorial);
+        txtID.setText(String.valueOf(objEditorial.getIdEditorial()));
+
+        TextView txtRuc = row.findViewById(R.id.DetalleRucEditorial);
+        txtRuc.setText(String.valueOf(objEditorial.getRuc()));
+
+        TextView txtDireccion = row.findViewById(R.id.DetalleDireccionEditorial);
+        txtDireccion.setText(String.valueOf(objEditorial.getDireccion()));
+
+       TextView txtCategoria = row.findViewById(R.id.DetalleCategoria);
+       txtCategoria.setText(objEditorial.getCategoria().getDescripcion());
+
+        TextView txtPais = row.findViewById(R.id.DetallePais);
+        txtPais.setText(objEditorial.getPais().getNombre());
+
+        TextView txtRazonSocial = row.findViewById(R.id.DetalleRazonSocialEditorial);
+        txtRazonSocial.setText(objEditorial.getRazonSocial());
+
+        return row;
     }
 
 }
--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/consulta/EditorialConsultaDetalleActivity.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/consulta/EditorialConsultaDetalleActivity.java
@@ -1,14 +1,51 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.vista.consulta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.cibertec.movil_modelo_proyecto_2022_2.R;
+import com.cibertec.movil_modelo_proyecto_2022_2.entity.Editorial;
 import com.cibertec.movil_modelo_proyecto_2022_2.util.NewAppCompatActivity;
 
 public class EditorialConsultaDetalleActivity extends NewAppCompatActivity {
+
+    TextView txtIdEditorial, txtRazonSocial;
+    TextView txtDireccionEditorial, txtRucEditorial, txtPaisEditorial, txtCategoriaEditorial;
+
+    Button btnVolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editorial_consulta_detalle);
+
+        txtIdEditorial = findViewById(R.id.txtIdEditorial);
+        txtRazonSocial = findViewById(R.id.txtRazonSocial);
+        txtDireccionEditorial = findViewById(R.id.txtDireccionEditorial);
+        txtPaisEditorial = findViewById(R.id.txtPaisEditorial);
+        txtCategoriaEditorial = findViewById(R.id.txtCategoriaEditorial);
+        txtRucEditorial = findViewById(R.id.txtRucEditorial);
+
+        Bundle extras = getIntent().getExtras();
+        Editorial objEditorial = (Editorial) extras.get("Var_Objeto");
+
+        txtIdEditorial.setText(objEditorial.getIdEditorial());
+        txtRazonSocial.setText(objEditorial.getRazonSocial());
+        txtDireccionEditorial.setText("Dirección: " + objEditorial.getDireccion());
+        txtPaisEditorial.setText("Pais: " + objEditorial.getPais().getNombre());
+        txtRucEditorial.setText("RUC: " + objEditorial.getRuc());
+        txtCategoriaEditorial.setText("Categoria: " + objEditorial.getCategoria().getDescripcion());
+
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EditorialConsultaDetalleActivity.this, EditorialConsultaActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
