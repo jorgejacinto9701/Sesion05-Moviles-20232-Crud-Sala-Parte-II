0f6ed67c101237ec54c9cf49c5fc36939af99f28
FRANCOIS TORRES <francoistorres07@gmail.com>
2023-05-25 23:23:04 -0500

    cambio de autor

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/service/ServiceAutor.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/service/ServiceAutor.java
@@ -1,4 +1,11 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.service;
 
+import com.cibertec.movil_modelo_proyecto_2022_2.entity.Autor;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+
 public interface ServiceAutor {
+
 }
