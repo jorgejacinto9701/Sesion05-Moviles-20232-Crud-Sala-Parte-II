d622d80bef45637c6171dc8aa41df0722ae1db5f
CristianAquice <c23200112@gmail.com>
2023-06-20 23:22:32 -0500

    Commit 1

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/service/ServiceProveedor.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/service/ServiceProveedor.java
@@ -8,6 +8,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public interface ServiceProveedor {
     @POST("proveedor")
@@ -15,4 +16,7 @@ public interface ServiceProveedor {
 
     @GET("proveedor")
     public abstract Call<List<Proveedor>> listaProveedor();
+
+    @PUT("proveedor")
+    public abstract Call<Proveedor> actualizaProveedor(@Body Proveedor objProveedor);
 }
--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/crud/EditorialCrudFormularioActivity.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/crud/EditorialCrudFormularioActivity.java
@@ -1,16 +1,43 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.vista.crud;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.cibertec.movil_modelo_proyecto_2022_2.R;
 import com.cibertec.movil_modelo_proyecto_2022_2.util.NewAppCompatActivity;
 
 public class EditorialCrudFormularioActivity extends NewAppCompatActivity {
 
+    Button btnCrudRegresarEditorial, btnCrudRegistrarEditorial;
+    TextView txtTitulo;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editorial_crud_formulario);
+
+        btnCrudRegistrarEditorial = findViewById(R.id.btnCrudRegistrarEditorial);
+        txtTitulo = findViewById(R.id.idCrudEditorial);
+        Bundle extras = getIntent().getExtras();
+        String tipo = (String) extras.get("var_tipo");
+        String titulo = (String) extras.get("var_titulo");
+
+        btnCrudRegistrarEditorial.setText(tipo);
+        txtTitulo.setText(titulo);
+
+        btnCrudRegresarEditorial = findViewById(R.id.btnCrudRegresarEditorial);
+        btnCrudRegresarEditorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditorialCrudFormularioActivity.this, EditorialCrudListaActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
