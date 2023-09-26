0c89435767b2a0e774e8692525041ee4ab709a52
nashdevcode <nash.devcode@gmail.com>
2023-09-25 19:47:30 -0600

    Spinner y Registrar

--- app/src/main/java/com/cibertec/proyecto/entity/Alumno.java
+++ app/src/main/java/com/cibertec/proyecto/entity/Alumno.java
@@ -2,6 +2,116 @@ package com.cibertec.proyecto.entity;
 
 public class Alumno {
 
+    private String nombres;
+    private String apellidos;
+    private String telefono;
+    private String dni;
+
+    private String fechaNacimiento;
+    private String fechaRegistro;
+    private int estado;
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(String fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    private String correo;
+
+    private String direccion;
+
+
+    public Pais getPaises() {
+        return paises;
+    }
+
+    public void setPaises(Pais paises) {
+        this.paises = paises;
+    }
+
+    private Pais paises;
+
+
+    public Modalidad getModalidades() {
+        return modalidades;
+    }
+
+    public void setModalidades(Modalidad modalidades) {
+        this.modalidades = modalidades;
+    }
+
+    private Modalidad modalidades;
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+
+
+
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 
 
 }
