02b89f72c25480f39d828e98bade3a96885713fc
Jeymi Goicochea <85200331+JeymiWick@users.noreply.github.com>
2023-10-12 00:23:40 -0500

    Final de formulario Consulta Autor

--- app/src/main/java/com/cibertec/proyecto/adapter/AutorAdapter.java
+++ app/src/main/java/com/cibertec/proyecto/adapter/AutorAdapter.java
@@ -1,10 +1,18 @@
 package com.cibertec.proyecto.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.bumptech.glide.Glide;
+import com.cibertec.proyecto.R;
 import com.cibertec.proyecto.entity.Autor;
 
 import java.util.List;
@@ -19,5 +27,36 @@ public class AutorAdapter extends ArrayAdapter<Autor>  {
         this.context = context;
         this.lista = lista;
     }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.activity_autor_crud_item, parent, false);
+
+        Autor objProducto = lista.get(position);
+
+        TextView txtID = row.findViewById(R.id.txtID);
+        txtID.setText(String.valueOf(objProducto.getIdAutor()));
+
+        TextView txtNombre = row.findViewById(R.id.txtNombre);
+        txtNombre.setText(objProducto.getNombres());
+
+        TextView txtApellido = row.findViewById(R.id.txtApellido);
+        txtApellido.setText(objProducto.getApellidos());
+
+        TextView txtCorreo = row.findViewById(R.id.txtCorreo);
+        txtCorreo.setText(objProducto.getCorreo());
+
+        TextView txtFechaNac = row.findViewById(R.id.txtFechaNac);
+        txtFechaNac.setText(objProducto.getFechaNacimiento());
+
+        TextView txtCelular = row.findViewById(R.id.txtCelular);
+        txtCelular.setText(objProducto.getTelefono());
+
+        return row;
+    }
+
+
 
 }
