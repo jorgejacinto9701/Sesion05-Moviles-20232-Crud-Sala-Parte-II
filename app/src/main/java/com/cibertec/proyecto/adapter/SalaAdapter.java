f8c7182db80a3c98443e5a886df2cda336c02891
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-10-17 21:10:06 -0500

    solucion

--- app/src/main/java/com/cibertec/proyecto/adapter/SalaAdapter.java
+++ app/src/main/java/com/cibertec/proyecto/adapter/SalaAdapter.java
@@ -1,12 +1,17 @@
 package com.cibertec.proyecto.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.cibertec.proyecto.entity.Sala;
-
+import com.cibertec.proyecto.R;
 
 import java.util.List;
 
@@ -21,4 +26,25 @@ public class SalaAdapter extends ArrayAdapter<Sala>  {
         this.lista = lista;
     }
 
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.activity_sala_consulta_item, parent, false);
+
+        Sala objSala = lista.get(position);
+
+        TextView txtID = row.findViewById(R.id.txtId);
+        txtID.setText("ID: " + String.valueOf(objSala.getIdSala()));
+
+        TextView txtNumero = row.findViewById(R.id.txtNumero);
+        txtNumero.setText("Número: " + objSala.getNumero());
+
+        TextView txtSede = row.findViewById(R.id.txtSede);
+        txtSede.setText("Sede: " + objSala.getSede().getNombre());
+
+
+        return row;
+    }
 }
--- app/src/main/java/com/cibertec/proyecto/service/ServiceSala.java
+++ app/src/main/java/com/cibertec/proyecto/service/ServiceSala.java
@@ -1,4 +1,17 @@
 package com.cibertec.proyecto.service;
 
+import com.cibertec.proyecto.entity.Autor;
+import com.cibertec.proyecto.entity.Sala;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface ServiceSala {
+
+    @GET("sala/porNumero/{numero}")
+    public Call<List<Sala>> listaSala (@Path("numero") String numero);
+
 }
