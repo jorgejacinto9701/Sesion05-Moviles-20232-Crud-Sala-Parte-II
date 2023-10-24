d83dcaa5c059eee2bd2f1d427980f8899ca52b02
nashdevcode <nash.devcode@gmail.com>
2023-10-24 17:07:56 -0500

    alumnoConsulta

--- app/src/main/java/com/cibertec/proyecto/adapter/AlumnoAdapter.java
+++ app/src/main/java/com/cibertec/proyecto/adapter/AlumnoAdapter.java
@@ -1,11 +1,18 @@
 package com.cibertec.proyecto.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.cibertec.proyecto.R;
 import com.cibertec.proyecto.entity.Alumno;
+
 
 
 import java.util.List;
@@ -20,5 +27,46 @@ public class AlumnoAdapter extends ArrayAdapter<Alumno>  {
         this.context = context;
         this.lista = lista;
     }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.activity_alumno_consulta_item, parent,false);
+
+        Alumno objAlumno = lista.get(position);
+
+        TextView txtID = row.findViewById(R.id.txtId);
+        txtID.setText("ID : " + String.valueOf(objAlumno.getIdAlumno()));
+
+        TextView txtNombres = row.findViewById(R.id.txtNombres);
+        txtNombres.setText("Nombre : " + objAlumno.getNombres());
+
+        TextView txtApellidos = row.findViewById(R.id.txtApellidos);
+        txtApellidos.setText("Apellidos : " + objAlumno.getApellidos());
+
+        TextView txtTelefono = row.findViewById(R.id.txtTelefono);
+        txtTelefono.setText("Serie : " + objAlumno.getTelefono());
+
+        TextView txtDni = row.findViewById(R.id.txtDni);
+        txtDni.setText("Dni : " + objAlumno.getDni());
+
+        TextView txtFechaNacimiento = row.findViewById(R.id.txtFechaNacimiento);
+        txtFechaNacimiento.setText("Fecha Nacimiento : " + objAlumno.getFechaNacimiento());
+
+        TextView txtCorreo = row.findViewById(R.id.txtCorreo);
+        txtCorreo.setText("Correo : " + objAlumno.getCorreo());
+
+        TextView txtPais = row.findViewById(R.id.txtPais);
+        txtPais.setText("País : " + objAlumno.getPais().getNombre());
+
+        TextView txtModalidad = row.findViewById(R.id.txtModalidad);
+        txtModalidad.setText("Modalidad : " + objAlumno.getModalidad().getDescripcion());
+
+        return row;
+
+    }
+
+
+
 
 }
--- app/src/main/java/com/cibertec/proyecto/service/ServiceAlumno.java
+++ app/src/main/java/com/cibertec/proyecto/service/ServiceAlumno.java
@@ -1,18 +1,26 @@
 package com.cibertec.proyecto.service;
 
 import com.cibertec.proyecto.entity.Alumno;
+import com.cibertec.proyecto.entity.Libro;
 
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 
 public interface ServiceAlumno {
+
+    @GET("alumno/porNombre/{nombre}")
+    public Call<List<Alumno>> listaalumno(@Path("nombre") String alumno);
+
+
 
 
     @POST("alumno")
     public abstract Call<Alumno> insertaAlumno (@Body Alumno objAlumno);
 }
-
-
