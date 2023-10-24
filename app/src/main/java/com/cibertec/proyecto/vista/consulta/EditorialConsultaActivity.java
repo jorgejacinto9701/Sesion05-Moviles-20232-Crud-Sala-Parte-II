5b6f345a119929a9c5de8537454f533c6d6e86c5
jhanio <jhaniohernandez@outlook.com>
2023-10-23 19:07:04 -0500

    PC-2 Creando layout -adapter-consulta

--- app/src/main/java/com/cibertec/proyecto/vista/consulta/EditorialConsultaActivity.java
+++ app/src/main/java/com/cibertec/proyecto/vista/consulta/EditorialConsultaActivity.java
@@ -1,12 +1,28 @@
 package com.cibertec.proyecto.vista.consulta;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.cibertec.proyecto.R;
+import com.cibertec.proyecto.adapter.EditorialAdapter;
+import com.cibertec.proyecto.entity.Editorial;
+import com.cibertec.proyecto.entity.Sala;
+import com.cibertec.proyecto.service.ServiceEditorial;
+import com.cibertec.proyecto.service.ServiceSala;
 import com.cibertec.proyecto.util.NewAppCompatActivity;
+
+import java.util.ArrayList;
 
 public class EditorialConsultaActivity extends NewAppCompatActivity {
 
+    Button btnListar;
+    ListView lstEditorial;
+    ArrayList<Editorial> data = new ArrayList<>();
+    EditorialAdapter adapter;
+    ServiceEditorial serviceEditorial;
+    EditText txtFiltro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
